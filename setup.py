80d1e6c1558725db31e7a12aaa7f2dd0b0fa894f
Steven Leibrock <steven.leibrock@gmail.com>
2015-07-02 15:04:40 -0400

    Updating setup to use a dict instead

--- setup.py
+++ setup.py
@@ -11,16 +11,22 @@ import mkterm.info
 
 here = path.abspath(path.dirname(__file__))
 
-setup(
-        name="mkterm",
-        version=mkterm.info.__version__,
-        description=mkterm.info.__description__,
-        long_description="Make multiple terminals on your desktop",
-        url=mkterm.info.__url__,
-        author=mkterm.info.__author__,
-        author_email=mkterm.info.__email__,
-        license=mkterm.info.__license__,
-        classifiers=[
+long_desc = """
+Rapidly spawn terminals on your desktop. Intended for users 
+with Tiling Window Managers who would like to populate a workspace
+with terminals
+"""
+
+conf = {
+        "name":"mkterm",
+        "version":mkterm.info.__version__,
+        "description":mkterm.info.__description__,
+        "long_description":long_desc,
+        "url":mkterm.info.__url__,
+        "author":mkterm.info.__author__,
+        "author_email":mkterm.info.__email__,
+        "license":mkterm.info.__license__,
+        "classifiers":[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
             "Programming Language :: Python :: 2",
@@ -31,16 +37,17 @@ setup(
             "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             ],
-        keywords="terminal emulator mkterm desktop",
-        packages=["mkterm"],
-        install_requires=[],
-        extras_require={},
-        package_data={},
-        entry_points={
+        "keywords":"terminal emulator mkterm desktop",
+        "packages":["mkterm"],
+        "install_requires":[],
+        "extras_require":{},
+        "package_data":{},
+        "entry_points":{
             'console_scripts':[
                 'mkterm=mkterm.app:main',
-                ],
-            }
-)
+                ]
+        }
+    }
 
+setup(**conf)
 # end
